64d4fc594e27e2a0e80aece0487077b12b55040f
Rex Feng <rexfeng@gmail.com>
2013-02-22 23:30:00 -0500

    add basic 4sq oauth sequence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,36 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :provider, type: String
   field :uid, type: String
+  field :token, type: String
+  field :expires_at, type: Integer
+
   field :first_name, type: String
   field :last_name, type: String
-  field :token, type: String
+  field :nickname, type: String
+  field :email, type: String
+  field :image, type: String
+
+  def self.from_omniauth(auth)
+    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  end
+
+  def self.create_from_omniauth(auth)
+    create! do |user|
+      
+      user.provider   = auth.provider
+      user.token      = auth.credentials.token
+      user.uid        = auth.uid
+      user.nickname   = auth.info.nickname
+      user.email      = auth.info.email
+      user.first_name = auth.info.first_name
+      user.last_name  = auth.info.last_name
+      user.image      = auth.info.image
+      user.expires_at = auth.credentials.expires_at
+
+    end
+  end
+
 end
