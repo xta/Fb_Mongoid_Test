30cbd0b431672ae13c9d6b6ce76181bb60a0cab6
Rex Feng <rexfeng@gmail.com>
2013-02-22 22:35:04 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'mongoid'
+gem 'jquery-rails'
+
+# fb specific
+  gem 'omniauth-facebook'
+  gem 'koala'
+
+group :assets do
+  gem 'uglifier', '>= 1.0.3'
+  gem 'pry-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbTestApp::Application.config.secret_token = '9c5e499f3621bd8c656a949767bc1de28252eedc1ce6e09ca522cc30899f4e51d9c471c5c105a8633d2fe6ca1fec517c2d12d50ee38d2d2ae1c2b00832d72e46'
